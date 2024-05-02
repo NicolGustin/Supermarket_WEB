c6805b55840073c33d15c4c5d225e21088d544f5
Nicol Gustin <nicol930519@hotmail.com>
2024-05-01 19:11:32 -0500

    Migrations

---
+++ SupermarketWEB/Data/SumermarketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SumermarketContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ SupermarketWEB/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } //Será la llave primaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; } //Será la llave foranea
+        public Category Category { get; set; }//Propiedad de navegacion 
+    }
+}
