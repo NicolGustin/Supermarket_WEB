381b796c045294435c90d4fdce3c8c458c705392
Nicol Gustin <nicol930519@hotmail.com>
2024-05-05 17:42:19 -0500

    Commit

--- SupermarketWEB/Pages/PayModes/Create.cshtml.cs
+++ SupermarketWEB/Pages/PayModes/Create.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.PayModes
 {
