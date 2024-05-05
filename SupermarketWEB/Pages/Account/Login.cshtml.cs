cd788953c31a6ff8323def9ab56936f7ba98380d
Nicol Gustin <nicol930519@hotmail.com>
2024-05-05 14:38:06 -0500

    Login

---
+++ SupermarketWEB/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Security.Claims;
+
+namespace SupermarketWEB.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        [BindProperty]
+        public Usuario Usuario { get; set; }
+
+        public LoginModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var registro = await _context.Registros.FirstOrDefaultAsync(r => r.Email == Usuario.Email && r.Password == Usuario.Password);
+
+            if (registro != null)
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, registro.Email),
+            };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            ModelState.AddModelError(string.Empty, "Credenciales inválidas.");
+            return Page();
+        }
+}
